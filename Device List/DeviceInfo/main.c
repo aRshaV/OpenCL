dcacea34a5cf4ff6e9cacfd74772d7ca8db23f00
Álvaro Simões <alvarez_simm_7@hotmail.com>
2017-08-02 13:11:11 +0100

    Device info main

---
+++ Device List/DeviceInfo/main.c
@@ -0,0 +1,33 @@
+//
+//  main.c
+//  OpenCL Devices Info
+//
+//  Created by aRshaV23 on 02/08/17.
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <OpenCL/opencl.h>
+
+int main(int argc, char* const argv[]) {
+    
+    cl_uint num_devices, i;
+    clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices);
+    
+    cl_device_id* devices = calloc(sizeof(cl_device_id), num_devices);
+    clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
+    
+    char buf[128];
+    for (i = 0; i < num_devices; i++) {
+        printf("Device number %d:\n",i+1);
+        clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 128, buf, NULL);
+        fprintf(stdout, "Device %s supports ", buf);
+        
+        clGetDeviceInfo(devices[i], CL_DEVICE_VERSION, 128, buf, NULL);
+        fprintf(stdout, "%s\n", buf);
+    }
+    
+    free(devices);
+    
+}
